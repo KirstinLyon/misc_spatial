92d332cbac6ed488d5b77f4241f450f5fb0ed495
kirstinlyon <kirstin@mltwelve.com>
2024-03-02 08:11:23 +0100

    Updated output folder so it is set at the top of the script.

--- Scripts/which_district.R
+++ Scripts/which_district.R
@@ -1,7 +1,6 @@
 # List of required packages
 required_packages <- c("tidyverse", "sf")
 
-# todo:  how to handle packages that are installed on GitHub and not CRAN?
 # Check if packages are installed
 missing_packages <- required_packages[!(required_packages %in% installed.packages()[,"Package"])]
 
@@ -24,6 +23,7 @@ unzip("Data/districts.zip", exdir = "Data/")
 
 shape_file <- "Data/districts/NEW_161_Districts.shp"
 coordinate_file <- "Data/sisma_us_gis.xlsx"
+output_folder <- "Dataout/"
 
 
 #' Use the SF package to find the snu and psnu for each coordinate. Flag any psnu that have 
@@ -40,36 +40,36 @@ check_coords_in_psnu <- function(shape_file, coordinate_file){
     
     # Read the shapefile containing district boundaries
     districts <- sf::st_read(shape_file) %>% 
-        select(shape_snu = Province, 
-               shape_psnu = District, 
-               shape_snuuid = SNU1Uid, 
+        select(shape_snu     = Province, 
+               shape_psnu    = District, 
+               shape_snuuid  = SNU1Uid, 
                shape_psnuuid = PSN_Uuid,
                geometry)
     
     # Read the Excel file containing facility coordinates
-    
     facilities_file <- readxl::read_xlsx(coordinate_file)
     
-    # Combine longitude and latitude into a single column
+    # prepare the facilities data and remove any sites without longitude/latitude
     facilities <- facilities_file %>%
         select(sitename,longitude, latitude,organisationunitid,
-               sisma_snuuid = snuuid,
+               sisma_snuuid  = snuuid,
                sisma_psnuuid = psnuuid,
-               sisma_snu = orgunitlevel2,
-               sisma_psnu = orgunitlevel3) %>% 
+               sisma_snu     = orgunitlevel2,
+               sisma_psnu    = orgunitlevel3) %>% 
         tidyr::drop_na()
 
     
-    # Convert facilities dataframe to sf object
+    # Convert facilities dataframe to sf object 
     facilities_sf <- sf::st_as_sf(x = facilities, 
                                   coords = c("longitude", "latitude"), 
                                   crs = st_crs(districts)) 
     
     
-    # Perform spatial join between districts and facilities
-    district_info <- st_join(districts, facilities_sf)%>% 
+    # Perform spatial join between districts and facilities and remove the geometry column
+    district_info <- st_join(districts, facilities_sf) %>% 
         sf::st_drop_geometry()
     
+    #add a flag to check if the psnu is as expected
     output_file <- district_info %>% 
         mutate(check_psnu = case_when(sisma_psnuuid == shape_psnuuid ~ 0,
                                       .default = 1)
@@ -83,11 +83,11 @@ check_coords_in_psnu <- function(shape_file, coordinate_file){
 
 
 all_data <- check_coords_in_psnu(shape_file, coordinate_file) %>% 
-    write_excel_csv("Dataout/all_data.csv")
+    write_excel_csv(glue::glue("{output_folder}all_data.csv"))
 
 wrong_psnu <- all_data %>% 
     filter(check_psnu == 1) %>% 
-    write_excel_csv("Dataout/check_psnu_gis.csv")
+    write_excel_csv(glue::glue("{output_folder}check_psnu_gis.csv"))
 
 
 
